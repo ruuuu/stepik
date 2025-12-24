ba2637f6695270c5c883b1b73df2b80a50f60953
ruuuu <rufinka_91@mail.ru>
2025-12-24 15:02:09 +0300

    Add files via upload

--- контекст в стрелочных функуиях/контекст в завимоси от вызова/bind_Call_Apply/4.js
+++ контекст в стрелочных функуиях/контекст в завимоси от вызова/bind_Call_Apply/4.js
@@ -1,25 +1,37 @@
 // Пример 8: Стрелочная функция + call/apply
 
-// РАЗОБРАТЬ!!
+
+// стрелочные игнорируют call/apply/bind
+
+
 
 const car = { brand: 'Toyota' };
 const bike = { brand: 'Yamaha' };
 
-const showBrand = () => {
+
+const showBrand = () => {       // стрлочная фкнция
   console.log(this.brand);
 };
 
-const showBrandRegular = function() {
+
+
+const showBrandRegular = function() {     // обычная фукнция
   console.log(this.brand);
 };
 
-showBrand.call(car);           // Вопрос: что выведет?
-showBrand.apply(bike);         // Вопрос: что выведет?
-showBrandRegular.call(car);    // Вопрос: что выведет?
-showBrandRegular.apply(bike);  // Вопрос: что выведет?
 
-const boundArrow = showBrand.bind(car);
-const boundRegular = showBrandRegular.bind(bike);
 
-boundArrow();                  // Вопрос: что выведет?
-boundRegular();                // Вопрос: что выведет?
+
+showBrand.call(car);           // стрелочные игнорируют call/apply, выведет undefined
+showBrand.apply(bike);         // стрелочные игнорируют call/apply, выведет undefined
+showBrandRegular.call(car);    // call вызывает showBrandRegular с this = car, выведет 'Toyota'
+showBrandRegular.apply(bike);  // call вызывает showBrandRegular с this = bike, выведет 'Yamaha'
+
+
+
+
+const boundArrow = showBrand.bind(car);     // стрелочные игнорируют call/apply/bind, выведет undefined
+const boundRegular = showBrandRegular.bind(bike);     // вернет функцию showBrandRegular с this = bike навсегда фиксированным, выведет 'Yamaha'
+
+boundArrow();                  // выведет undefined
+boundRegular();                // выведет 'Yamaha'
