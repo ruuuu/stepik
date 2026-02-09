dded029583207723de577dcddf8702d85573e4b6
ruuuu <rufinka_91@mail.ru>
2026-02-09 17:42:51 +0300

    Add files via upload

--- промисы/promises/promice_any/1.js
+++ промисы/promises/promice_any/1.js
@@ -1,15 +1,53 @@
 // Пример 1: Первый успешный промис
 
-const promise1 = Promise.reject('Ошибка 1');
-const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'Успех 2'));
-const promise3 = new Promise((resolve) => setTimeout(resolve, 200, 'Успех 3'));
+
+// const promise1 = Promise.reject('Ошибка 1');
+// const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'Успех 2'));
+// const promise3 = new Promise((resolve) => setTimeout(resolve, 200, 'Успех 3'));
+
+// Promise.any([promise1, promise2, promise3])
+//   .then((result) => {          // в result запишется результат промиса Promise.any
+//     console.log('Первый успешный:', result); // "Успех 2"
+//   })
+//   .catch((error) => {
+//     console.error('Все промисы отклонены:', error);
+//   });
 
 
 
-Promise.any([promise1, promise2, promise3])
+
+
+// const promise1 = Promise.reject('Ошибка 1');        // промис синхрнно отклонится с ошибкой "Ошибка 1'"
+// const promise2 = new Promise((resolve) => setTimeout(() => resolve("Успех2"), 400));       // промис разрешится с результатом "Успех2"
+// const promise3 = new Promise((_, reject) => setTimeout(() => reject("Ошибка3"), 200));     // промис отклонится с результатом "Ошибка3"
+
+
+
+
+// Promise.any([promise1, promise2, promise3])    // вернет первый успешно выполнившийся промис, в result сохранится результ этог промиса 
+//   .then((result) => {
+//     console.log('Первый успешный:', result);      // "Успех 2"
+//   })
+//   .catch((error) => {     // в error запишется результаты всех выполнившихся с ошибкой прмисов
+//     console.error('Все промисы отклонены:', error);
+//   });
+
+
+
+
+// Пример когда все промисы отклонены: 
+
+const promise1 = Promise.reject('Ошибка 1');        // промис синхрнно(сразу) отклонится с ошибкой "Ошибка 1'"
+const promise2 = new Promise((_, reject) => setTimeout(() => reject("Ошибка2"), 400));
+const promise3 = new Promise((_, reject) => setTimeout(() => reject("Ошибка3"), 200));
+
+
+
+
+Promise.any([promise1, promise2, promise3])    // вернет первый успешно выполнившийся промис, в result сохранится результ этог промиса 
   .then((result) => {
-    console.log('Первый успешный:', result); // "Успех 2"
+    console.log('Первый успешный:', result);      // 
   })
-  .catch((error) => {
+  .catch((error) => {     // в error запишется результаты всех выполнившихся с ошибкой прмисов
     console.error('Все промисы отклонены:', error);
   });
--- промисы/promises/promice_any/3.js
+++ промисы/promises/promice_any/3.js
@@ -6,18 +6,20 @@
 
 // Получение данных из нескольких источников (используется первый ответивший)
 
-const fastSource = fetch('https://api.fast.com/data').catch(() => 'timeout');
-const reliableSource = fetch('https://api.reliable.com/data').catch(() => 'timeout');
-const backupSource = fetch('https://api.backup.com/data').catch(() => 'timeout');
+const fastSource = fetch('https://httpbin.org/brotli').catch(() => 'timeout');
+const reliableSource = fetch('https://httpbin.org/deflate').catch(() => 'timeout');
+const backupSource = fetch('https://httpbin.org/html').catch(() => 'timeout');
 
 
+//  console.log(Promise.any([fastSource, reliableSource, backupSource]))  // Promise { <pending> }
 
-Promise.any([fastSource, reliableSource, backupSource])
-  .then((response) => {
+Promise.any([fastSource, reliableSource, backupSource])   //  вернет первый успешно выполнвшийся ромис
+  .then((response) => {   // в response передастся то,  что вернет Promise.any(его результат)
     console.log('Данные получены от самого быстрого источника');
+    console.log('response ', response)
     return response.json();
   })
-  .then((data) => {
+  .then((data) => {     //  в data передастся то что вернет предыддущий then()
     console.log('Данные:', data);
   })
   .catch(() => {
